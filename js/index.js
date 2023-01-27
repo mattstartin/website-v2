8f74be791c7d6715d8ff84ba72bb946bf6d0b283
Matt Startin <m.j.startin+github@googlemail.com>
2023-01-27 23:31:19 +0000

    Colour Randomiser locking better styled

--- js/index.js
+++ js/index.js
@@ -25,40 +25,47 @@ function themeClick() {
     var subColour = Math.floor(Math.random()*16777215).toString(16);
 
 
-    if(document.getElementById('schema-bg').innerText != "L") {
+    if(document.getElementById('schema-bg').dataset.locked != 'true') {
         bodyStyles.setProperty('--background', '#'+bgColour);
         document.getElementById('schema-bg').setAttribute('style', 'background-color: #' + bgColour);
     }
     
-    if(document.getElementById('schema-tint').innerText != "L") {
+    if(document.getElementById('schema-tint').dataset.locked != 'true') {
         bodyStyles.setProperty('--background-tint2', '#'+tintColour);
         document.getElementById('schema-tint').setAttribute('style', 'background-color: #' + tintColour);
     }
     
-    if(document.getElementById('schema-hl').innerText != "L") {
+    if(document.getElementById('schema-hl').dataset.locked != 'true') {
         bodyStyles.setProperty('--text-highlight', '#'+hlColour);
         document.getElementById('schema-hl').setAttribute('style', 'background-color: #' + hlColour);
     }
 
-    if(document.getElementById('schema-cont').innerText != "L") {
+    if(document.getElementById('schema-cont').dataset.locked != 'true') {
         bodyStyles.setProperty('--text-contrast', '#'+contColour);
         document.getElementById('schema-cont').setAttribute('style', 'background-color: #' + contColour);
     }
 
-    if(document.getElementById('schema-main').innerText != "L") {
+    if(document.getElementById('schema-main').dataset.locked != 'true') {
         bodyStyles.setProperty('--text-main', '#'+mainColour);
         document.getElementById('schema-main').setAttribute('style', 'background-color: #' + mainColour);
     }
 
-    if(document.getElementById('schema-sub').innerText != "L") {
+    if(document.getElementById('schema-sub').dataset.locked != 'true') {
         bodyStyles.setProperty('--text-sub', '#'+subColour);
         document.getElementById('schema-sub').setAttribute('style', 'background-color: #' + subColour);
     }
 }
 
 function paletteClick(el) {
-    if(document.getElementById(el).innerText != "L") 
-        document.getElementById(el).innerText = "L"
-    else
-        document.getElementById(el).innerText = ""
+    const element = document.getElementById(el);
+    if(element.dataset.locked =='true') {
+        element.dataset.locked = 'false'
+        element.children[0].classList.remove('fa-lock')
+        element.children[0].classList.add('fa-lock-open')
+    }
+    else {
+        element.dataset.locked = 'true'
+        element.children[0].classList.remove('fa-lock-open')
+        element.children[0].classList.add('fa-lock')
+    }
 }
